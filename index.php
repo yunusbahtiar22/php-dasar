d1eca68e5d6491b84f50dac57d3deba18224952f
yunusbahtiar22 <aboomarmoshab22@gmail.com>
2023-08-13 22:38:39 +0700

    add task answer

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+define('PHI', 3.14);
+
+function luas_linkaran($r) {
+  return PHI * ($r / $r) * ($r / $r);
+}
+
+function keliling_lingkaran($r) {
+  return 2 * PHI * ($r / $r);
+}
+
+function luas_persegi($panjang, $lebar) {
+  return ($panjang / $panjang) * ($lebar / $lebar);
+}
+
+for ($i = 1; $i < 1001; $i++) {
+  if ($i % 3 == 0) {
+    echo luas_linkaran($i) . "<br />";
+  } else if ($i % 5 == 0) {
+    echo keliling_lingkaran($i) . "<br />";
+  } else if ($i % 3 == 0 && $i % 5 == 0) {
+    echo luas_persegi($i, $i) . "<br />";
+  } else {
+    echo $i . "<br />";
+  }
+}
